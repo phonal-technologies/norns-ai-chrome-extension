5fe1a000f3d5920a71b739c3f0e2e7e109707700
Dmitry Tuzenkov <dmitry.tuzenkov@gmail.com>
2020-07-27 21:46:57 +0300

    feat(integration): Initial commit

--- src/content.js
+++ src/content.js
@@ -1,1 +1,11 @@
 console.log('Injected in Norns.ai')
+console.log(window.location.href)
+
+function hashHandler() {
+  console.log('The hash has changed!');
+}
+// TODO: should match new - old 
+window.addEventListener('hashchange', hashHandler, false);
+window.onpopstate = function(event) {
+  alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
+};
